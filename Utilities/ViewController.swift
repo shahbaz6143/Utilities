b492e1400bd43e676fbf566a95c84f4a8053cc85
Shahbaz Alam <63611678+shahbaz6143@users.noreply.github.com>
2020-12-11 23:51:03 +0530

    Update ViewController.swift

--- Utilities/ViewController.swift
+++ Utilities/ViewController.swift
@@ -18,4 +18,36 @@ class ViewController: UIViewController {
 
 }
 
+extension String {
+    
+    var isPhoneNumber: Bool {
+        do {
+            let detector = try NSDataDetector(types: NSTextCheckingResult.CheckingType.phoneNumber.rawValue)
+            let matches = detector.matches(in: self, options: [], range: NSMakeRange(0, self.count))
+            if let res = matches.first {
+                return res.resultType == .phoneNumber && res.range.location == 0 && res.range.length == self.count && self.count == 10
+            } else {
+                return false
+            }
+        } catch {
+            
+            return false
+        }
+    }
+    
+    var isPasswordValid: Bool {
+        
+        let passwordTest = NSPredicate(format: "SELF MATCHES %@", "^(?=.*[a-z])(?=.*[$@$#!%*?&])[A-Za-z\\d$@$#!%*?&]{8}")
+        return passwordTest.evaluate(with: self)
+    }
+    
+    
+    
+    var isValidEmail: Bool {
+        
+        let emailTest = NSPredicate(format: "SELF MATCHES %@", "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,3}")
+        return emailTest.evaluate(with: self)
+    }
+}
+
 
